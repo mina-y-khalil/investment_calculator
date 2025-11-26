cc80927dfbb1cc30c0f0a12ece1fbc123fc901c7
mina.y.khalil <mina.y.khalil90@gmail.com>
2025-11-26 13:09:17 -0800

    add investment calculator program

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
-
+import (
+	"math" // we need math package for power function there is no built-in power operator in Go
+	"fmt" // this package is used for printing output to the console
+) 
 
 func main () {
-	fmt.Print("Investment Calculator")
-}
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow((1 + expectedReturnRate /100) , float64(float64(years)))
+	fmt.Println("Future Value of Investment: ", futureValue) // the ln after println is to print the output to the console in new line
+}  
