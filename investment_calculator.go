14f4fe142f982a0ea0bbaa7872dcfbac4c82d452
mina.y.khalil <mina.y.khalil90@gmail.com>
2025-11-29 16:04:42 -0800

    Using Alternative Variable Declaration Styles

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,8 +6,7 @@ import (
 ) 
 
 func main () {
-	var investmentAmount, years float64 = 1000, 10
-	expectedReturnRate := 5.5 // we removed the var and replaced it with := for type inference we can do this only when we are initializing the variable without using var keyword
+	var investmentAmount, years, expectedReturnRate  := 1000.0 , 10.0, 5.5
 
 	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate /100) , years)
 	fmt.Println("Future Value of Investment: ", futureValue) // the ln after println is to print the output to the console in new line
