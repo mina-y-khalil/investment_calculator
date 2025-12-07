d403a261f46e7fed6ef75a7fd14395ade21eb27c
mina.y.khalil <mina.y.khalil90@gmail.com>
2025-12-06 22:01:18 -0800

    add user inputs func.

--- investment_calculator.go
+++ investment_calculator.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"math" // we need math package for power function there is no built-in power operator in Go
-	"fmt" // this package is used for printing output to the console
+	"fmt" // this package is used for printing output to the console this also get import from terminal
 ) 
 
 func main () {
 	const inflationRate = 2.5
-	var investmentAmount float64 = 1000	
+	var investmentAmount float64  // this value will be overridden by user input that is why we are not assigning any value here
 	expectedReturnRate := 5.5
-	var years float64 = 10
+	var years float64
+
+
+	// user inputs
+	fmt.Print("Enter the investment amount: ")
+	fmt.Scan(&investmentAmount) // pointers to read input from user (single word input)
+
+	fmt.Print("Expected annual return rate (in %): ")
+	fmt.Scan(&expectedReturnRate) // pointers to read input from user
+
+	fmt.Print("Years to invest: ")
+	fmt.Scan(&years) // pointers to read input from user
 
 	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate /100) , years)
 	futureRealValue := futureValue /  math.Pow((1 + inflationRate /100) , years)
